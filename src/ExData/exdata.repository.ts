ca24aa68df9a6da148134ab561f77481f57a6452
dhlee-alticast <dh.lee1@alticast.com>
2021-03-30 15:57:55 +0900

    소스 refactoring

--- src/ExData/exdata.repository.ts
+++
@@ -1,7 +0,0 @@
-import { Repository } from 'typeorm';
-import { EntityRepository } from 'typeorm/decorator/EntityRepository';
-
-import { ExData } from './exdata.entity';
-
-@EntityRepository(ExData)
-export class ExDAtaRepository extends Repository<ExData> {}
